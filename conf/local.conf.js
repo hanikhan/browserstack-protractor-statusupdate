58c91d6ee66e81d49d68d05bb4249d6e658ea8b4
Hani <Hani@Hanis-MBP.local>
2018-04-15 00:49:15 +0530

    changes to implement sample configuration for status update- local tests

--- conf/local.conf.js
+++ conf/local.conf.js
@@ -1,12 +1,11 @@
 var browserstack = require('browserstack-local');
 
 exports.config = {
+   browserstackUser:'BROWSERSTACK_USERNAME',
+   browserstackKey:'BROWSERSTACK_ACCESS_KEY',
   'specs': [ '../specs/local.js' ],
-  'seleniumAddress': 'http://hub-cloud.browserstack.com/wd/hub',
 
   'capabilities': {
-    'browserstack.user': process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
-    'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
     'build': 'protractor-browserstack',
     'name': 'local_test',
     'browserName': 'chrome',
@@ -19,7 +18,7 @@ exports.config = {
     console.log("Connecting local");
     return new Promise(function(resolve, reject){
       exports.bs_local = new browserstack.Local();
-      exports.bs_local.start({'key': exports.config.capabilities['browserstack.key'] }, function(error) {
+      exports.bs_local.start({'key': exports.config.capabilities['browserstack.key'] , 'force':true}, function(error) {
         if (error) return reject(error);
         console.log('Connected. Now testing...');
 
